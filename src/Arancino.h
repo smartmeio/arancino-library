2a688ada72d180a2cfb2f526493e288cfa35b16d
sergio <sergio@smartme.io>
2019-08-26 18:13:29 +0200

    update tabs

--- src/Arancino.h
+++ src/Arancino.h
@@ -77,36 +77,48 @@ class ArancinoClass {
 	public:
 		//void begin();
 		void begin(int timeout = TIMEOUT);
-        void startScheduler();
+    	void startScheduler();
 		void setReservedCommunicationMode(int mode);
-        ArancinoPacket getPacket(char* value);
-        String get(char* value);
+    	
+		ArancinoPacket getPacket(char* value);
+    	String get(char* value);
+		
 		ArancinoPacket set(char* key, char* value);
 		ArancinoPacket set(char* key, int value);
 		ArancinoPacket set(char* key, double value);
 		ArancinoPacket set(char* key, uint32_t value);
+		
 		ArancinoPacket delPacket(char* key);
 		int del(char* key);
+		
 		ArancinoPacket keysPacket(char* pattern="");
 		String* keys(char* pattern="");
+		
 		ArancinoPacket hgetPacket(char* key, char* field);
 		String hget(char* key, char* field);
+		
 		ArancinoPacket hgetallPacket(char* key);
 		String* hgetall(char* key);
+		
 		ArancinoPacket hkeysPacket(char* key);
 		String* hkeys(char* key);
+		
 		ArancinoPacket hvalsPacket(char* key);
 		String* hvals(char* key);
+		
 		ArancinoPacket hset(char* key, char* field, char* value);
 		ArancinoPacket hset(char* key, char* field, int value);
 		ArancinoPacket hset(char* key, char* field, double value);
 		ArancinoPacket hset(char* key, char* field, uint32_t value);
+		
 		ArancinoPacket hdelPacket(char* key, char* field);
 		int hdel(char* key, char* field);
+		
 		ArancinoPacket publishPacket(char* channel, char* msg);
 		int publish(char* channel, char* msg);
 		ArancinoPacket publishPacket(int channel, char* msg);
 		int publish(int channel, char* msg);
+		
 		ArancinoPacket flush(void);
 
 		void print(String value);
@@ -116,36 +128,48 @@ class ArancinoClass {
 		void println(String value);
 		void println(int value);
 		void println(double value);
-        int getArraySize(char** _array);
-        int getArraySize(String* _array);
-        void freeArray(char** _array);
-        void freeArray(String* _array);
-        void freePacket(ArancinoPacket packet);
+		
+		int getArraySize(char** _array);
+		int getArraySize(String* _array);
+		
+		void freeArray(char** _array);
+		void freeArray(String* _array);
+		void freePacket(ArancinoPacket packet);
 		//int hdel( String key, String* fields, int number);
 		//ArancinoClass(Stream &_stream);
 
 	private:
 		//void dropAll();
 		bool started;
-        int getResponseCode(char* data);
+		int getResponseCode(char* data);
+		
 		char* parse(char* message);
 		char** parseArray(char* message);
+		
 		void sendArancinoCommand(String command);
 		void sendArancinoCommand(char* command);
 		void sendArancinoCommand(char command);
-        char* receiveArancinoResponse(char terminator);
+		
+		char* receiveArancinoResponse(char terminator);
+		
 		bool isReservedKey(String key);
-        bool isReservedKey(char* key);
+		bool isReservedKey(char* key);
+		
 		char reservedKey[4][11]; //max 10 char for key
+		
 		//Stream &stream;
-        void doubleToString(double value, unsigned int _nDecimal, char* str); //truncation!
-        int getDigit(long value);
-        int COMM_MODE = SYNCH;
+		
+		void doubleToString(double value, unsigned int _nDecimal, char* str); //truncation!
+		int getDigit(long value);
+		
+		int COMM_MODE = SYNCH;
+		
 		ArancinoPacket sendViaCOMM_MODE(char* key, char* value);
 		ArancinoPacket _set(char* key, char* value);
-        ArancinoPacket _publish(char* channel, char* msg);
-        const char dataSplitStr[2] = {DATA_SPLIT_CHAR, '\0'};
-        const char endTXStr[2] = {END_TX_CHAR, '\0'};
+		ArancinoPacket _publish(char* channel, char* msg);
+		
+		const char dataSplitStr[2] = {DATA_SPLIT_CHAR, '\0'};
+		const char endTXStr[2] = {END_TX_CHAR, '\0'};
 };
 
 extern ArancinoClass Arancino;
